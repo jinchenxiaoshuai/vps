71a2fafe08c0e1d4bef2a61da3cc57122f5d1e27
gengziyuan <gengziyuan1124@gmail.com>
2018-08-09 10:46:43 +0800

    更新指令下载方式

--- download_ssr.sh
+++ download_ssr.sh
@@ -1,1 +1,9 @@
-wget -N --no-check-certificate https://softs.fun/Bash/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+# wget -N --no-check-certificate https://softs.fun/Bash/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+
+cd "/usr/local/"
+wget -N --no-check-certificate "https://github.com/Paranea-TM/vps/archive/master.zip"
+[[ ! -e "vps-master.zip" ]] && echo -e "${Error} ShadowsocksR可执行脚本 下载失败 !" && rm -rf vps-master.zip && exit 1
+unzip "vps-master.zip"
+[[ ! -e "/usr/local/vps-master/" ]] && echo -e "${Error} ShadowsocksR可执行脚本 解压失败 !" && rm -rf vps-master.zip && exit 1
+mv "/usr/local/vps-master/" "/usr/local/vps/"
